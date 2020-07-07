6f78846ce9294437cbcc19166784c2e3edd5a2b8
Simba-brother <2196664077@qq.com>
2020-07-07 09:53:21 +0800

    demo

---
+++ demo/src/main/java/com/example/demo/bean/UserBean.java
@@ -0,0 +1,40 @@
+package com.example.demo.bean;
+
+public class UserBean {
+    private int id;
+    private String name;
+    private String password;
+    private String info;
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ demo/src/main/java/com/example/demo/controller/LoginController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import com.example.demo.bean.UserBean;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ */
+
+@Controller
+public class LoginController {
+    //将Service注入Web层
+    @Autowired
+    UserService userService;
+
+    @RequestMapping("/login")
+    public String show(){
+        return "login";
+    }
+    @RequestMapping(value = "/loginIn",method = RequestMethod.POST)
+    public String login(String name,String password) {  //前端点击登陆按钮
+        UserBean userBean = userService.loginIn(name, password);  //调用service层
+        if (userBean != null) {
+            return "success";
+        } else {
+            return "error";
+        }
+    }
+}
