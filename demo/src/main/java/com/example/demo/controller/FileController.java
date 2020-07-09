b0f85c487d1f7c1ed025be627f7a4b3382fdb7e0
Simba-brother <2196664077@qq.com>
2020-07-09 13:54:35 +0800

    "fastJson处理"

--- demo/src/main/java/com/example/demo/controller/FileController.java
+++ demo/src/main/java/com/example/demo/controller/FileController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletResponse;
 import com.alibaba.fastjson.JSON;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -18,11 +19,12 @@ public class FileController {
     FileService fileService;
 
     @RequestMapping("/fileJson")
+    @ResponseBody
     public String getJson(HttpServletResponse response) throws IOException {
         String html = fileService.getLabel();
-        response.setCharacterEncoding("utf-8");
-        PrintWriter respWritter = response.getWriter();
-        respWritter.write(html);
-        return "success";
+//        response.setCharacterEncoding("utf-8");
+//        PrintWriter respWritter = response.getWriter();
+//        respWritter.write(html);
+        return html;
     }
 }
